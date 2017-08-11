c162509584fd89ec74993b8ba18957b07d961a50
Val Wong <valentin.wong@gmail.com>
2017-08-10 18:13:42 -0700

    removed start

--- db/schema.sql
+++ db/schema.sql
@@ -8,5 +8,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
